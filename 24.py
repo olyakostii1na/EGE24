64896dce0d3e18b538b5290277410f958e3849a9
Olya <okot30652@gmail.com>
2022-07-01 13:27:10 +0300

    95

--- 24.py
+++ 24.py
@@ -1,9 +1,9 @@
-file = open('k8-1.txt')
+file = open('24-1.txt')
 s = file.readline()
 k = 0
 max_len = 0
-for i in range(len(s) - 2):
-    if s[i] != s[i - 1] != s[i + 1]:
+for i in range(len(s) - 1):
+    if s[i] < s[i + 1] and s[i] > s[i - 1]:
         k += 1
         max_len = max(k, max_len)
 print(max_len)
